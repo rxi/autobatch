08dbe90920f90f14183eca4d9fc860ffa77ca415
rxi <rxi@users.noreply.github.com>
2018-03-10 11:37:33 +0000

    Updated copyright year; 2016 -> 2018

--- autobatch.lua
+++ autobatch.lua
@@ -1,7 +1,7 @@
 --
 -- autobatch.lua
 --
--- Copyright (c) 2016 rxi
+-- Copyright (c) 2018 rxi
 --
 -- This library is free software; you can redistribute it and/or modify it
 -- under the terms of the MIT license. See LICENSE for details.
